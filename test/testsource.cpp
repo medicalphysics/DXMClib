6877d9d8de29598978862459606f68f1ad52b6f5
erlend andersen <andersen.erlend@gmail.com>
2020-01-06 15:37:04 +0100

    first commit

---
+++ test/testsource.cpp
@@ -0,0 +1,26 @@
+ 
+
+
+#include "source.h"
+#include <iostream>
+#include <vector>
+
+
+//test CTDIPHANTOM
+
+int testCTDIPhantom(void)
+{
+
+
+
+
+
+
+    return 0;
+}
+
+
+int main (int argc, char *argv[])
+{
+
+}
