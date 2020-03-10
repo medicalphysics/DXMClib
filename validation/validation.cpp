0cc119325be55744480f4172456da6d2b1025cd3
Erlend Andersen <andersen.erlend@gmail.com>
2020-03-10 22:16:11 +0100

    prepare TG195 validation

--- validation/validation.cpp
+++ validation/validation.cpp
@@ -1,11 +1,22 @@
 
 
-#include
+#include "dxmc/tube.h"
 
 
 #include <iostream>
 
+
+bool TG195CompareSpecter(double kV = 120)
+{
+	return false;
+}
+
+
+
 int main(int argc, char* argv[])
 {
 
+	TG195CompareSpecter(); // compare with TG195
+
+	return 1;
 }
