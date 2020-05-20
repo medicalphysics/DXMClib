aed71c7272cbbdf42255b4de46405a8942b7f6ec
erlend andersen <andersen.erlend@gmail.com>
2020-05-20 15:06:56 +0200

    small fixes

--- tests/testvectormath.cpp
+++ tests/testvectormath.cpp
@@ -19,9 +19,28 @@ void testArgMax()
 	return;
 }
 
+void testChangeBasis()
+{
+	std::array<double, 6> cos{ 1,0,0,0,1,0 };
+	std::array<double, 3> dir;
+	vectormath::cross(cos.data(), dir.data());
+
+	std::array<double, 3> pos{ 1,0,0 };
+	auto posFin = pos;
+
+	const double* b1 = &cos[0];
+	const double* b2 = &cos[3];
+	const double* b3 = &dir[0];
+
+	vectormath::changeBasisInverse(b1, b2, b3, posFin.data());
+
+	for (std::size_t i = 0; i < 3; ++i)
+		assert((pos[i] == posFin[i]));
+}
 
 int main(int argc, char* argv[])
 {
 	testArgMax();
+	testChangeBasis();
 	return EXIT_SUCCESS;
 }
