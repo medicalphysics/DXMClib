73abe235aff7efc728ab1d400f520382988a4cac
erlend andersen <andersen.erlend@gmail.com>
2020-10-21 10:37:59 +0200

    updated example

--- examples/pencilbeam/pencilbeam.cpp
+++ examples/pencilbeam/pencilbeam.cpp
@@ -15,7 +15,6 @@ using namespace dxmc;
 template <typename T>
 double calculate()
 {
-
     // Lets create a world that describes our voxelized model to score dose in.
     World<T> world;
     // We need to specify dimensions and voxel spacing (in millimeters)
@@ -24,11 +23,11 @@ double calculate()
     std::array<T, 3> spacing = { 1.0, 1.0, 1.0 };
     world.setSpacing(spacing);
 
-    // Now we fill the world box with som materials
+    // Now we fill the world box with some materials
     // We specify three materials
-    Material air("Air, Dry (near sea level)"); // Material air("N0.76O0.23Ar0.01") is eqvivalent
-    Material water("Water, Liquid"); // Material water("H2O") is eqvivalent
-    Material aluminium(13); // aluminium
+    Material air("Air, Dry (near sea level)"); // Material air("N0.76O0.23Ar0.01") is equivalent
+    Material water("Water, Liquid"); // Material water("H2O") is equivalent
+    Material aluminium(13); // Material aluminum
 
     // Lets add the materials to the world material map
     world.addMaterialToMap(air);
@@ -39,61 +38,69 @@ double calculate()
     auto arraySize = world.size();
     // the type of density array is double
     auto densityArray = std::make_shared<std::vector<T>>(arraySize);
-    // the type of material index array is unsigned char (up to 255 materials are supported)
-    auto materialIndexArray = std::make_shared<std::vector<unsigned char>>(arraySize);
+    // the type of material index array is std::uint8_t or old school unsigned char (up to 255 materials are supported)
+    auto materialIndexArray = std::make_shared<std::vector<std::uint8_t>>(arraySize);
     // Now fill the arrays
     for (std::size_t i = 0; i < dimensions[0]; ++i)
         for (std::size_t j = 0; j < dimensions[1]; ++j)
             for (std::size_t k = 0; k < dimensions[2]; ++k) {
                 auto index = i + j * dimensions[0] + k * dimensions[0] * dimensions[1];
-                if (k < dimensions[2] / 3) {
+                if (k < dimensions[2] / 3) { // The first 1/3 part of the box is air.
                     densityArray->data()[index] = air.standardDensity();
-                    ;
                     materialIndexArray->data()[index] = 0;
-                } else if (k < dimensions[2] * 2 / 3) {
+                } else if (k < dimensions[2] * 2 / 3) { // The second 1/3 part of the box is water.
                     densityArray->data()[index] = water.standardDensity();
                     materialIndexArray->data()[index] = 1;
-                } else {
+                } else { // The third 1/3 part of the box is aluminum.
                     densityArray->data()[index] = aluminium.standardDensity();
                     materialIndexArray->data()[index] = 2;
                 }
             }
-
+    // Add the density array and the material index array to world.
     world.setDensityArray(densityArray);
     world.setMaterialIndexArray(materialIndexArray);
+    // The world is now complete, to test if it is valid and all material indices are correct
+    // and arrays have correct dimensions we can call:
+    world.makeValid();
+    auto valid = world.isValid();
 
-    // we need a beam source
+    // We need a beam source, in this case a simple pencil beam
     PencilSource<T> pen;
 
-    //pencilsource only support monochromatic intensity
+    //PencilSource only support monochromatic intensity, in this case we use 60 keV initial photon energy
     pen.setPhotonEnergy(60.0); // keV
 
-    //position the source above the world box
-    //std::array<double, 3> source_position = { 0,0, -dimensions[2] * spacing[2] };
-    std::array<T, 3> source_position = { 0, 0, 0 };
+    //Position the source above the world box
+    std::array<T, 3> source_position = { 0, 0, -(dimensions[2] * spacing[2]) };
     pen.setPosition(source_position);
-    // we want the cross product aka the beam direction to point towards the box
-    std::array<T, 6> beam_cosines = { -1, 0, 0, 0, 1, 0 };
+    // We want the source plane normal aka the beam direction to point towards the box
+    // To set the direction of the beam we must specify the beam direction cosines.
+    // This is basically two orthonormal vectors on the beam plane [x, y].
+    std::array<T, 6> beam_cosines = { 1, 0, 0, 0, 1, 0 };
     pen.setDirectionCosines(beam_cosines);
-    pen.setTotalExposures(10000); // number of batches
-    pen.setHistoriesPerExposure(10000); // histories per batch
+    // The beam direction is then x cross y
+    std::array<T, 3> beam_direction;
+    vectormath::cross(beam_cosines.data(), beam_direction.data());
+    // The beam direction should be [0, 0, 1]
 
-    //setting normalizing DAP value
-    pen.setAirDose(1.0);
-    //run simulation
-    world.makeValid();
+    // Specify number of exposures, each exposure is run on a single thread
+    // so make sure there are more exposures than cores on your CPU for optimal performance.
+    // For CT sources, number of exposures are determined by the geometry.
+    pen.setTotalExposures(200);
+    // Set total number of histories per exposure
+    pen.setHistoriesPerExposure(1000000);
 
+    // Create a transport object
     Transport<T> transport;
-    ProgressBar<T> progressBar;
-
+    // Do the simulation, this might take some time depending on total number of histories.
     auto res = transport(world, &pen);
-    auto dose = res.dose;
-    auto time = std::chrono::duration_cast<std::chrono::milliseconds>(res.simulationTime).count() / 1000.0;
 
-    //printing dose dose along z direction of box
+    //Print the dose along z direction of box
     auto materials = world.materialMap();
+    auto time = std::chrono::duration_cast<std::chrono::milliseconds>(res.simulationTime).count() / 1000.0;
     std::cout << "Simulation time, " << time << ", seconds\n";
     std::cout << "Depth [mm],  Dose, nEvents, Material index, Material\n";
+    auto dose = res.dose;
     for (std::size_t k = 0; k < dimensions[2]; ++k) {
         std::cout << spacing[2] * k << ", ";
         const auto offset = dimensions[0] * dimensions[1];
@@ -114,6 +121,11 @@ int main(int argc, char* argv[])
 {
     auto n_secs_d = calculate<double>();
     auto n_secs_f = calculate<float>();
-
+    std::cout << "Simulation time for double precision: ";
+    std::cout << n_secs_d << " seconds\n";
+    std::cout << "Simulation time for single precision: ";
+    std::cout << n_secs_f << " seconds\n";
+    std::cout << "Single precision was " << std::ceil(100 - n_secs_f / n_secs_d * 100);
+    std::cout << " percent faster\n";
     return 1;
 }
