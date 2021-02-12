074d95776e0e29b2d9b03517df5c78054643ab15
Erlend Andersen <andersen.erlend@gmail.com>
2021-02-12 13:50:38 +0100

    Removed generation incusion of doxygen docs in readthedocs build due to compability issue with c++20

--- docs/sphinx/conf.py
+++ docs/sphinx/conf.py
@@ -28,8 +28,8 @@ author = 'Erlend Andersen'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["breathe", "exhale"
-]
+#extensions = ["breathe", "exhale"
+#]
 
 # Breathe Configuration
 breathe_default_project = "DXMClib"
