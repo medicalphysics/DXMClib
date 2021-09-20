982b4efe4e87299b7279b9ad20cdb895aa9b8199
Erlend Andersen <andersen.erlend@gmail.com>
2021-09-20 22:16:06 +0200

    Made plots prettier

--- validation/validationPlots.py
+++ validation/validationPlots.py
@@ -24,72 +24,77 @@ import numpy as np
 def readData():    
     converters={"Result":float, "Stddev":float}
     dt = pd.read_csv("validationTable.txt", sep=", ", engine='python', converters=converters, error_bad_lines=False)
-    
+    nNaN= dt.isnull().sum().sum()
+    if nNaN > 0:
+        print("Warning: Found {} NaN values or missing data, dropping rows".format(nNaN))
+        return dt.dropna()
     return dt
 
+def fix_axis(fg, rotate_labels=True):
+    fg.set(ylim=(0, None), ylabel="Energy [eV/history]")
+    if rotate_labels:
+        for ax in fg.axes_dict.values():             
+            for tick in ax.get_xticklabels():
+                tick.set_rotation(70)
 
 def plotCase2(dt_full):
     dt=dt_full[dt_full['Case']=="Case 2"]    
     dt_vol=dt[dt['Volume'] != 'Total body']
     for forced in [0, 1]:
         dt_f=dt_vol[dt_vol['Forced']==forced]
-        g = sns.catplot(x="Volume", y="Result [ev/hist]",hue='Model', row='Specter', col='Mode', data=dt_f )
-        g.set(ylim=(0, None))
+        g = sns.catplot(x="Volume", y="Result",hue='Model', row='Specter', col='Mode', data=dt_f )
+        fix_axis(g)
         plt.savefig("Case2_volume{}.png".format("_forced" if forced==1 else ""), dpi=900)
         plt.show()
         plt.clf()
 
     dt_tot = dt[dt['Volume'] == "Total body"]
-    g = sns.catplot(x="Forced", y="Result [ev/hist]",hue='Model', row='Specter', col='Mode', data=dt_tot )
-    g.set(ylim=(0, None))
+    g = sns.catplot(x="Forced", y="Result",hue='Model', row='Specter', col='Mode', data=dt_tot )
+    fix_axis(g, False)
     plt.savefig("Case2_total_body.png", dpi=900)
     plt.show()
     plt.clf()
-
-    
-
+ 
 def plotCase3(dt_full):
     dt=dt_full[dt_full['Case']=="Case 3"]    
     dt_vol=dt[dt['Volume'] != 'Total body']
     for forced in [0, 1]:
         dt_f=dt_vol[dt_vol['Forced']==forced]
-        sns.catplot(x="Volume", y="Result [ev/hist]",hue='Model', row='Specter', col='Mode', data=dt_f )
+        sns.catplot(x="Volume", y="Result",hue='Model', row='Specter', col='Mode', data=dt_f )
         g = plt.savefig("Case3_volume{}.png".format("_forced" if forced==1 else ""), dpi=900)
-        g.set(ylim=(0, None))
+        fix_axis(g)       
         plt.show()
         plt.clf()
 
     dt_tot = dt[dt['Volume'] == "Total body"]
-    g = sns.catplot(x="Forced", y="Result [ev/hist]",hue='Model', row='Specter', col='Mode', data=dt_tot )
-    g.set(ylim=(0, None))
+    g = sns.catplot(x="Forced", y="Result",hue='Model', row='Specter', col='Mode', data=dt_tot )
+    fix_axis(g, False)
     plt.savefig("Case3_total_body.png", dpi=900)
     plt.show()
     plt.clf()
 
-    
 def plotCase41(dt_full):
     dt=dt_full[dt_full['Case']=="Case 4.1"]        
-    g = sns.catplot(x="Volume", y="Result [ev/hist]",hue='Model', row='Specter', col='Mode', data=dt )
-    g.set(ylim=(0, None))
+    g = sns.catplot(x="Volume", y="Result",hue='Model', row='Specter', col='Mode', data=dt )
+    fix_axis(g, False)
     plt.savefig("Case41.png", dpi=900)
     plt.show()
     
 def plotCase42(dt_full):
     dt_42=dt_full[dt_full['Case']=="Case 4.2"]
-    
     for forced in [1, 0]:
         dt = dt_42[dt_42['Forced'] == forced]
         dtp_ind=['Cent' in e for e in dt['Mode']]
         dtp = dt[dtp_ind]
-        g = sns.catplot(x="Volume", y="Result [ev/hist]",hue='Model', row='Specter', col='Mode', data=dtp )
-        g.set(ylim=(0, None))
+        g = sns.catplot(x="Volume", y="Result",hue='Model', row='Specter', col='Mode', data=dtp )
+        fix_axis(g)
         plt.savefig("Case42_Cent{}.png".format("_forced" if forced==1 else ""), dpi=900)
         plt.show()
     
         dtp_ind=['Pher' in e for e in dt['Mode']]
         dtp = dt[dtp_ind]
-        g = sns.catplot(x="Volume", y="Result [ev/hist]",hue='Model', row='Specter', col='Mode', data=dtp )
-        g.set(ylim=(0, None))
+        g = sns.catplot(x="Volume", y="Result",hue='Model', row='Specter', col='Mode', data=dtp )
+        fix_axis(g)
         plt.savefig("Case42_Pher{}.png".format("_forced" if forced==1 else ""), dpi=900)
         plt.show()
     
@@ -98,11 +103,10 @@ def plotCase5(dt_full):
     vols = set([m for m in dt['Volume']])
     for m in vols:
         dtm = dt[dt['Volume']==m]
-        g = sns.catplot(x="Mode", y="Result [ev/hist]",hue='Model', row=None, col='Specter', data=dtm )
-        g.set(ylim=(0, None))
+        g = sns.catplot(x="Mode", y="Result",hue='Model', row=None, col='Specter', data=dtm )
+        fix_axis(g)
         plt.savefig("Case5_{}.png".format(m), dpi=900)
         plt.show()
-
 
 if __name__=='__main__':
     dt = readData()
@@ -111,4 +115,3 @@ if __name__=='__main__':
     plotCase41(dt)
     plotCase42(dt)
     plotCase5(dt)
-
