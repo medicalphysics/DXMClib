91db7d10c1f083078207d576089a96820dcdd5d2
erlend andersen <andersen.erlend@gmail.com>
2023-08-23 23:15:09 +0200

    fix line prop

--- include/dxmc/world/visualization/vizualizationprops.hpp
+++ include/dxmc/world/visualization/vizualizationprops.hpp
@@ -54,44 +54,6 @@ namespace visualizationprops {
 
         std::optional<std::pair<T, std::array<T, 3>>> intersect(const Particle<T>& p) const noexcept override
         {
-            const auto v0 = vectormath::cross(vectormath::subtract(p.pos, m_pos), m_dir);
-            const auto v1 = vectormath::cross(p.dir, m_dir);
-
-            const auto a = vectormath::dot(v1, v1);
-            const auto b = 2 * vectormath::dot(v0, v1);
-            const auto c = vectormath::dot(v0, v0) - m_radii * m_radii;
-
-            const auto den = b * b - 4 * a * c;
-            if (den <= 0 || std::abs(a) < GEOMETRIC_ERROR<T>())
-                return std::nullopt;
-            const auto den_s = std::sqrt(den);
-            const auto t1 = (-b + den_s) / (2 * a);
-            const auto t2 = (-b - den_s) / (2 * a);
-            const auto t = std::min(t1, t2);
-
-            // finding normal
-            auto point = vectormath::add(p.pos, vectormath::scale(p.dir, t));
-            auto point_diff = vectormath::subtract(m_pos, point);
-            auto line_t_neg = vectormath::dot(point_diff, m_dir);
-
-            if (-line_t_neg > m_lenght) {
-                const auto ts = std::max(t1, t2);
-                point = vectormath::add(p.pos, vectormath::scale(p.dir, ts));
-                point_diff = vectormath::subtract(m_pos, point);
-                line_t_neg = vectormath::dot(point_diff, m_dir);
-                if (-line_t_neg > m_lenght) {
-                    return std::nullopt;
-                }
-            }
-
-            auto normal = vectormath::subtract(vectormath::scale(m_dir, line_t_neg), point_diff);
-            vectormath::normalize(normal);
-
-            return std::make_optional(std::make_pair(t, normal));
-        }
-
-        std::optional<std::pair<T, std::array<T, 3>>> intersect2(const Particle<T>& p) const noexcept // override
-        {
 
             const auto n = vectormath::cross(p.dir, m_dir);
             const auto n_lenght = vectormath::lenght(n);
@@ -99,7 +61,6 @@ namespace visualizationprops {
                 return std::nullopt;
             }
             const auto n_invLenght = 1 / n_lenght;
-
             const auto distance = std::abs(vectormath::dot(n, vectormath::subtract(m_pos, p.pos)) * n_invLenght);
 
             if (distance > m_radii) { // intersection is larger than radii
@@ -112,16 +73,16 @@ namespace visualizationprops {
             const auto n2 = vectormath::cross(m_dir, n);
             const auto t1 = vectormath::dot(vectormath::subtract(s_pos, p.pos), n2) / vectormath::dot(n2, p.dir);
 
-            // test lenght
             //  intersection pos
             const auto p_int = vectormath::add(p.pos, vectormath::scale(p.dir, t1));
-            // distance from intpos to line
-            const auto int_distance = std::abs(vectormath::dot(vectormath::subtract(p_int, m_pos), m_dir));
+            // distance from intpos to start
+            const auto int_distance = vectormath::dot(vectormath::subtract(p_int, m_pos), m_dir);
+            if (int_distance < T { 0 } || int_distance > m_lenght)
+                return std::nullopt;
 
             auto d = vectormath::subtract(p_int, p.pos);
             d = vectormath::subtract(d, vectormath::scale(m_dir, vectormath::dot(d, m_dir)));
             vectormath::normalize(d);
-
             return std::make_optional(std::make_pair(t1, d));
         }
 
