7e176de96972a112d7df7a48397623f2e7969f2b
erlend andersen <andersen.erlend@gmail.com>
2024-01-10 00:45:43 +0100

    branchless aabb intersection

--- include/dxmc/world/basicshapes/aabb.hpp
+++ include/dxmc/world/basicshapes/aabb.hpp
@@ -33,192 +33,75 @@ namespace basicshape {
     namespace AABB {
 
         template <Floating T>
-        bool pointInside(const std::array<T, 3>& p, const std::array<T, 6>& aabb)
+        inline constexpr bool pointInside(const std::array<T, 3>& p, const std::array<T, 6>& aabb)
         {
             return aabb[0] <= p[0] && p[0] <= aabb[3] && aabb[1] <= p[1] && p[1] <= aabb[4] && aabb[2] <= p[2] && p[2] <= aabb[5];
+        }
+
+        template <Floating T>
+        inline constexpr bool overlap(const std::array<T, 6>& a, const std::array<T, 6>& b)
+        {
+            const bool x = a[0] <= b[3] && a[3] >= b[0];
+            const bool y = a[1] <= b[4] && a[4] >= b[1];
+            const bool z = a[2] <= b[5] && a[5] >= b[2];
+            return x && y && z;
         }
 
         template <Floating T, bool FORWARD = true>
         std::optional<std::array<T, 2>> intersectForwardInterval(const Particle<T>& p, const std::array<T, 6>& aabb)
         {
-            std::array<T, 2> t;
+            const std::array<T, 3> pdir_inv = { 1 / p.dir[0], 1 / p.dir[1], 1 / p.dir[2] };
+            T t1 = (aabb[0] - p.pos[0]) * pdir_inv[0];
+            T t2 = (aabb[3] - p.pos[0]) * pdir_inv[0];
 
-            const auto dx = 1 / p.dir[0];
-            if (dx >= 0) {
-                t[0] = (aabb[0] - p.pos[0]) * dx;
-                t[1] = (aabb[3] - p.pos[0]) * dx;
-            } else {
-                t[0] = (aabb[3] - p.pos[0]) * dx;
-                t[1] = (aabb[0] - p.pos[0]) * dx;
+            std::array<T, 2> tm = { std::min(t1, t2), std::max(t1, t2) };
+
+            for (int i = 1; i < 3; ++i) {
+                t1 = (aabb[i] - p.pos[i]) * pdir_inv[i];
+                t2 = (aabb[i + 3] - p.pos[i]) * pdir_inv[i];
+
+                tm[0] = std::max(tm[0], std::min(std::min(t1, t2), tm[1]));
+                tm[1] = std::min(tm[1], std::max(std::max(t1, t2), tm[0]));
             }
-            T tymin, tymax;
-            const auto dy = 1 / p.dir[1];
-            if (dy >= 0) {
-                tymin = (aabb[1] - p.pos[1]) * dy;
-                tymax = (aabb[4] - p.pos[1]) * dy;
-            } else {
-                tymin = (aabb[4] - p.pos[1]) * dy;
-                tymax = (aabb[1] - p.pos[1]) * dy;
-            }
-            if ((t[0] > tymax) || (tymin > t[1]))
-                return std::nullopt;
-
-            if (tymin > t[0])
-                t[0] = tymin;
-            if (tymax < t[1])
-                t[1] = tymax;
-
-            T tzmin, tzmax;
-            const auto dz = 1 / p.dir[2];
-            if (dz >= 0) {
-                tzmin = (aabb[2] - p.pos[2]) * dz;
-                tzmax = (aabb[5] - p.pos[2]) * dz;
-            } else {
-                tzmin = (aabb[5] - p.pos[2]) * dz;
-                tzmax = (aabb[2] - p.pos[2]) * dz;
-            }
-
-            if ((t[0] > tzmax) || (tzmin > t[1]))
-                return std::nullopt;
-            if (tzmin > t[0])
-                t[0] = tzmin;
-            if (tzmax < t[1])
-                t[1] = tzmax;
-            if (t[1] <= 0)
-                return std::nullopt;
-            if constexpr (FORWARD) {
-                if (t[0] < T { 0 })
-                    t[0] = T { 0 };
-            }
-
-            return std::make_optional(t);
+            if constexpr (FORWARD)
+                tm[0] = std::max(tm[0], T { 0 });
+            return tm[1] > tm[0] ? std::make_optional(tm) : std::nullopt;
         }
 
-        // Branched version with early exits
         template <Floating T>
         WorldIntersectionResult<T> intersect(const Particle<T>& p, const std::array<T, 6>& aabb)
         {
             WorldIntersectionResult<T> res;
-            std::array<T, 2> t;
-
-            const auto dx = 1 / p.dir[0];
-            if (dx >= 0) {
-                t[0] = (aabb[0] - p.pos[0]) * dx;
-                t[1] = (aabb[3] - p.pos[0]) * dx;
-            } else {
-                t[0] = (aabb[3] - p.pos[0]) * dx;
-                t[1] = (aabb[0] - p.pos[0]) * dx;
+            if (const auto t_cand = intersectForwardInterval<T, true>(p, aabb); t_cand) {
+                const auto& t = *t_cand;
+                res.rayOriginIsInsideItem = t[0] <= 0;
+                res.intersection = res.rayOriginIsInsideItem ? t[1] : t[0];
+                res.intersectionValid = true;
             }
-            T tymin, tymax;
-            const auto dy = 1 / p.dir[1];
-            if (dy >= 0) {
-                tymin = (aabb[1] - p.pos[1]) * dy;
-                tymax = (aabb[4] - p.pos[1]) * dy;
-            } else {
-                tymin = (aabb[4] - p.pos[1]) * dy;
-                tymax = (aabb[1] - p.pos[1]) * dy;
-            }
-            if ((t[0] > tymax) || (tymin > t[1]))
-                return res;
-
-            if (tymin > t[0])
-                t[0] = tymin;
-            if (tymax < t[1])
-                t[1] = tymax;
-
-            T tzmin, tzmax;
-            const auto dz = 1 / p.dir[2];
-            if (dz >= 0) {
-                tzmin = (aabb[2] - p.pos[2]) * dz;
-                tzmax = (aabb[5] - p.pos[2]) * dz;
-            } else {
-                tzmin = (aabb[5] - p.pos[2]) * dz;
-                tzmax = (aabb[2] - p.pos[2]) * dz;
-            }
-
-            if ((t[0] > tzmax) || (tzmin > t[1]))
-                return res;
-            if (tzmin > t[0])
-                t[0] = tzmin;
-            if (tzmax < t[1])
-                t[1] = tzmax;
-            if (t[1] <= 0)
-                return res;
-
-            res.rayOriginIsInsideItem = t[0] <= 0;
-            res.intersection = res.rayOriginIsInsideItem ? t[1] : t[0];
-            res.intersectionValid = true;
-
             return res;
         }
 
-        // Branched version with early exits
         template <Floating T, typename U>
         VisualizationIntersectionResult<T, U> intersectVisualization(const Particle<T>& p, const std::array<T, 6>& aabb)
         {
             VisualizationIntersectionResult<T, U> res;
-            int axis_min = 0;
-            int axis_max = 0;
-
-            std::array<T, 2> t;
-
-            const auto dx = 1 / p.dir[0];
-            if (dx >= 0) {
-                t[0] = (aabb[0] - p.pos[0]) * dx;
-                t[1] = (aabb[3] - p.pos[0]) * dx;
-            } else {
-                t[0] = (aabb[3] - p.pos[0]) * dx;
-                t[1] = (aabb[0] - p.pos[0]) * dx;
+            if (const auto t_cand = intersectForwardInterval<T, true>(p, aabb); t_cand) {
+                const auto& t = *t_cand;
+                res.rayOriginIsInsideItem = t[0] <= 0;
+                res.intersection = res.rayOriginIsInsideItem ? t[1] : t[0];
+                res.intersectionValid = true;
+                std::array<T, 6> hit_merit;
+                for (int i = 0; i < 3; ++i) {
+                    const auto hit = p.pos[i] + res.intersection * p.dir[i];
+                    hit_merit[i] = std::abs(aabb[i] - hit);
+                    hit_merit[i + 3] = std::abs(aabb[i + 3] - hit);
+                }
+                const auto pos = std::distance(hit_merit.cbegin(), std::min_element(hit_merit.cbegin(), hit_merit.cend()));
+                if (pos < 3)
+                    res.normal[pos] = -1;
+                else
+                    res.normal[pos - 3] = 1;
             }
-            T tymin, tymax;
-            const auto dy = 1 / p.dir[1];
-            if (dy >= 0) {
-                tymin = (aabb[1] - p.pos[1]) * dy;
-                tymax = (aabb[4] - p.pos[1]) * dy;
-            } else {
-                tymin = (aabb[4] - p.pos[1]) * dy;
-                tymax = (aabb[1] - p.pos[1]) * dy;
-            }
-            if ((t[0] > tymax) || (tymin > t[1]))
-                return res;
-
-            if (tymin > t[0]) {
-                t[0] = tymin;
-                axis_min = 1;
-            }
-            if (tymax < t[1]) {
-                t[1] = tymax;
-                axis_max = 1;
-            }
-
-            T tzmin, tzmax;
-            const auto dz = 1 / p.dir[2];
-            if (dz >= 0) {
-                tzmin = (aabb[2] - p.pos[2]) * dz;
-                tzmax = (aabb[5] - p.pos[2]) * dz;
-            } else {
-                tzmin = (aabb[5] - p.pos[2]) * dz;
-                tzmax = (aabb[2] - p.pos[2]) * dz;
-            }
-
-            if ((t[0] > tzmax) || (tzmin > t[1]))
-                return res;
-            if (tzmin > t[0]) {
-                t[0] = tzmin;
-                axis_min = 2;
-            }
-            if (tzmax < t[1]) {
-                t[1] = tzmax;
-                axis_max = 2;
-            }
-            if (t[1] <= 0)
-                return res;
-
-            res.rayOriginIsInsideItem = t[0] <= 0;
-            res.intersection = res.rayOriginIsInsideItem ? t[1] : t[0];
-            res.intersectionValid = true;
-            const int axis = res.rayOriginIsInsideItem ? axis_max : axis_min;
-            res.normal[axis] = p.dir[axis] > 0 ? T { -1 } : T { 1 };            
             return res;
         }
     }
