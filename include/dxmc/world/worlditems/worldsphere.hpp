9ef71ae1e976ec3e2cff059e05cc8c8d361422e6
erlend andersen <andersen.erlend@gmail.com>
2024-02-15 14:01:48 +0100

    fixed forced interactions

--- include/dxmc/world/worlditems/worldsphere.hpp
+++ include/dxmc/world/worlditems/worldsphere.hpp
@@ -183,32 +183,38 @@ protected:
         bool updateAtt = false;
         auto att = m_material.attenuationValues(p.energy);
         auto attSum = att.sum() * m_materialDensity;
-        auto attSumInv = 1 / attSum;
         auto relativePeProbability = att.photoelectric / att.sum();
         while (cont) {
             if (updateAtt) {
                 att = m_material.attenuationValues(p.energy);
-                attSum = att.sum() * m_materialDensity;
-                attSumInv = 1 / attSum;
+                attSum = att.sum() * m_materialDensity;                
                 relativePeProbability = att.photoelectric / att.sum();
                 updateAtt = false;
             }
-            const auto stepLen = -std::log(state.randomUniform<T>()) * attSumInv; // cm
+            // const auto stepLen = -std::log(state.randomUniform<T>()) * attSumInv; // cm
             const auto intLen = intersect(p).intersection; // this must be valid
+            const auto probNotInteraction = std::exp(-attSum * intLen);
 
-            const auto photoProb = (1 - std::exp(-intLen * attSum)) * relativePeProbability;
-
-            // Forced photoelectric effect
-            m_energyScored.scoreEnergy(p.energy * p.weight * photoProb);
+            //Forced photoelectric effect
+            m_energyScored.scoreEnergy(p.energy * p.weight * (1 - probNotInteraction) * relativePeProbability);
 
             // Remainder probability
-            if (stepLen < intLen && state.randomUniform<T>() > relativePeProbability) {
-                // interaction happends
-                p.translate(stepLen);
-                const auto intRes = interactions::template interactScatter<T, NMaterialShells, LOWENERGYCORRECTION>(att, p, m_material, state);
-                m_energyScored.scoreEnergy(intRes.energyImparted);
-                cont = intRes.particleAlive;
-                updateAtt = intRes.particleEnergyChanged;
+            const auto p1 = state.randomUniform<T>();
+            if (p1 > probNotInteraction) {
+                if (state.randomUniform<T>() > relativePeProbability) {
+                    // scatter interaction happends
+                    const auto stepLen = -std::log(p1) / attSum;
+                    p.translate(stepLen);
+                    const auto intRes = interactions::template interactScatter<T, NMaterialShells, LOWENERGYCORRECTION>(att, p, m_material, state);
+                    m_energyScored.scoreEnergy(intRes.energyImparted);
+                    cont = intRes.particleAlive;
+                    updateAtt = intRes.particleEnergyChanged;
+                } else {
+                    // photoelectric effect
+                    // we don't score energy since it's done. But terminates particle to prevent bias.
+                    cont = false;
+                    p.energy = 0;
+                }
             } else {
                 // transport to border
                 p.border_translate(intLen);
@@ -218,7 +224,8 @@ protected:
     }
 
 private:
-    T m_radius = 0;
+    T m_radius
+        = 0;
     std::array<T, 3> m_center;
     Material<T, NMaterialShells> m_material;
     T m_materialDensity = 1;
--- tests/testworldsphere.cpp
+++ tests/testworldsphere.cpp
@@ -32,62 +32,14 @@ bool testForcedinteractions()
     using World = dxmc::World<T, Sphere>;
     using WorldF = dxmc::World<T, SphereF>;
 
-    World w;
-    WorldF wf;
-
-    constexpr T radii = 5;
-    auto& sphere = w.addItem<Sphere>({ radii });
-    auto& spheref = wf.addItem<SphereF>({ radii });
-
-    auto material_water = dxmc::Material<T, 5>::byNistName("Water, Liquid").value();
-    sphere.setMaterial(material_water, 1);
-    spheref.setMaterial(material_water, 1);
-
-    w.build();
-    wf.build();
-
-    dxmc::PencilBeam<T> beam({ -100, 0, 0 }, { 1, 0, 0 }, 20);
-    beam.setNumberOfExposures(124);
-    beam.setNumberOfParticlesPerExposure(1e4);
-
-    dxmc::Transport transport;
-    transport(wf, beam);
-    transport(w, beam);
-
-    auto dose = sphere.doseScored().dose();
-    auto dosef = spheref.doseScored().dose();
-
-    auto diff = std::abs(dose - dosef);
-
-    auto sttd = sphere.doseScored().standardDeviation();
-    auto sttdf = spheref.doseScored().standardDeviation();
-
-    std::cout << "Random " << sphere.doseScored().dose() << std::endl;
-    std::cout << "Forced " << spheref.doseScored().dose() << std::endl;
-    std::cout << "Diff: " << diff << ", sttdev: ";
-    std::cout << sttd << ", " << sttdf << std::endl;
-
-    return false;
-}
-
-template <dxmc::Floating T>
-bool testForcedinteractionsExits()
-{
-    using Sphere = dxmc::WorldSphere<T, 5, 1, false>;
-    using SphereF = dxmc::WorldSphere<T, 5, 1, true>;
-
-    using World = dxmc::World<T, Sphere>;
-    using WorldF = dxmc::World<T, SphereF>;
-
     World w(2);
     WorldF wf(2);
 
-
-    constexpr T radii = 1;
+    constexpr T radii = 0.3;
     auto& sphere1 = w.addItem<Sphere>({ radii });
-    auto& sphere2 = w.addItem<Sphere>({ radii, { radii * 2 + 1, 0, 0 } });
+    auto& sphere2 = w.addItem<Sphere>({ radii, { radii * 2 + radii * 100, 0, 0 } });
     auto& spheref1 = wf.addItem<SphereF>({ radii });
-    auto& spheref2 = wf.addItem<SphereF>({ radii, { radii * 2 + 1, 0, 0 } });
+    auto& spheref2 = wf.addItem<SphereF>({ radii, { radii * 2 + radii * 100, 0, 0 } });
 
     auto material_water = dxmc::Material<T, 5>::byNistName("Water, Liquid").value();
     sphere1.setMaterial(material_water, 1);
@@ -98,10 +50,9 @@ bool testForcedinteractionsExits()
     w.build();
     wf.build();
 
-
     dxmc::PencilBeam<T> beam({ -100, 0, 0 }, { 1, 0, 0 }, 20);
     beam.setNumberOfExposures(48);
-    beam.setNumberOfParticlesPerExposure(1e4);
+    beam.setNumberOfParticlesPerExposure(1e5);
 
     dxmc::Transport transport;
     transport(wf, beam);
@@ -121,19 +72,30 @@ bool testForcedinteractionsExits()
     auto sttd2 = sphere2.doseScored().standardDeviation();
     auto sttdf2 = spheref2.doseScored().standardDeviation();
 
+    std::cout << "Testing forced interactions in worldsphere for precision " << sizeof(T) << std::endl;
     std::cout << "Random " << dose1 << ", " << dose2 << " stddev: " << sttd1 << ", " << sttd2 << std::endl;
     std::cout << "Forced " << dosef1 << ", " << dosef2 << " stddev: " << sttdf1 << ", " << sttdf2 << std::endl;
     std::cout << "Diff: " << diff1 << ", " << diff2 << std::endl;
 
-    return false;
+    constexpr T test_coeff = T { 2.57 };
+
+    const auto test1 = diff1 / std::sqrt(sttd1 * sttd1 + sttdf1 * sttdf1);
+    const auto test2 = diff2 / std::sqrt(sttd2 * sttd2 + sttdf2 * sttdf2);
+
+    std::cout << test1 << ", " << test2 << std::endl;
+    auto success = (test1 < test_coeff) && (test2 < test_coeff);
+    if (success)
+        std::cout << "SUCCESS: differences within statistical limits" << std::endl;
+    else
+        std::cout << "FAILURE: differences outside statistical limits" << std::endl;
+    return success;
 }
 
 int main(int argc, char* argv[])
 {
     auto success = true;
 
-    success = success && testForcedinteractionsExits<double>();
-    //success = success && testForcedinteractions<float>();
+    success = success && testForcedinteractions<double>();
 
     if (success)
         return EXIT_SUCCESS;
